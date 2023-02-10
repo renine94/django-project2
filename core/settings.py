c18ff6d2da3a637e1e2b8cecb72d5c0a9fcc9b65
kyle <jg.kang@fitpet.co.kr>
2023-02-10 13:29:28 +0900

    add: url 수정 및 gitignore 추가, settings 에 rest_framework 추가

--- core/settings.py
+++ core/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -122,3 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'accounts.User'
